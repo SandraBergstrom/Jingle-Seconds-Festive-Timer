ee0e626e49874755e222143dbca93547a401e268
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-12-14 14:26:31 +0000

    Initial commit

---
+++ countdown.js
@@ -0,0 +1,25 @@
+const newYearDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function () {
+  // Get the current date and time
+  const now = new Date().getTime();
+
+  // Calculate the remaining time
+  const distance = newYearDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Happy New Year!";
+  }
+}, 1000);
---
+++ script.js
@@ -0,0 +1,89 @@
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYmFybmVzbG93IiwiYSI6ImNsMGUyeHV6MDBmMGYzanBybDIyZ3BvOTQifQ.orwWz3XDibvdJSe_tfAxEA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/satellite-v9",
+  projection: "globe", // Display the map as a globe, since satellite-v9 defaults to Mercator
+  zoom: 1.5,
+  center: [-90, 40],
+});
+
+map.on("style.load", () => {
+  map.setFog({}); // Set the default atmosphere style
+});
+
+// The following values can be changed to control rotation speed:
+
+// At low zooms, complete a revolution every two minutes.
+const secondsPerRevolution = 120;
+// Above zoom level 5, do not rotate.
+const maxSpinZoom = 5;
+// Rotate at intermediate speeds between zoom levels 3 and 5.
+const slowSpinZoom = 3;
+
+let userInteracting = false;
+let spinEnabled = true;
+
+function spinGlobe() {
+  const zoom = map.getZoom();
+  if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
+    let distancePerSecond = 360 / secondsPerRevolution;
+    if (zoom > slowSpinZoom) {
+      // Slow spinning at higher zooms
+      const zoomDif = (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
+      distancePerSecond *= zoomDif;
+    }
+    const center = map.getCenter();
+    center.lng -= distancePerSecond;
+    // Smoothly animate the map over one second.
+    // When this animation is complete, it calls a 'moveend' event.
+    map.easeTo({ center, duration: 1000, easing: (n) => n });
+  }
+}
+
+// Pause spinning on interaction
+map.on("mousedown", () => {
+  userInteracting = true;
+});
+
+// Restart spinning the globe when interaction is complete
+map.on("mouseup", () => {
+  userInteracting = false;
+  spinGlobe();
+});
+
+// These events account for cases where the mouse has moved
+// off the map, so 'mouseup' will not be fired.
+map.on("dragend", () => {
+  userInteracting = false;
+  spinGlobe();
+});
+map.on("pitchend", () => {
+  userInteracting = false;
+  spinGlobe();
+});
+map.on("rotateend", () => {
+  userInteracting = false;
+  spinGlobe();
+});
+
+// When animation is complete, start spinning if there is no ongoing interaction
+map.on("moveend", () => {
+  spinGlobe();
+});
+
+document.getElementById("btn-spin").addEventListener("click", (e) => {
+  spinEnabled = !spinEnabled;
+  if (spinEnabled) {
+    spinGlobe();
+    e.target.innerHTML = "Pause rotation";
+  } else {
+    map.stop(); // Immediately end ongoing animation
+    e.target.innerHTML = "Start rotation";
+  }
+});
+
+spinGlobe();
